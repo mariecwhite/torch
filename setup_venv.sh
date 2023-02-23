ee047f0a6d00adfb83518f7fbd13938598edb8ae
mariecwhite <mariewhite@google.com>
2023-02-22 17:26:24 -0800

    Fixes for gpu

--- setup_venv.sh
+++ setup_venv.sh
@@ -13,15 +13,13 @@ function die() {
   exit 1
 }
 
-$PYTHON -m venv "$VENV_DIR" || die "Could not create venv."
+python -m venv "$VENV_DIR" || die "Could not create venv."
 source "$VENV_DIR/bin/activate" || die "Could not activate venv"
 
-# Upgrade pip and install requirements. 'python' is used here in order to
-# reference to the python executable from the venv.
 python -m pip install --upgrade pip || die "Could not upgrade pip"
 
-$PYTHON -m pip install numpy --pre torch[dynamo] --force-reinstall --extra-index-url https://download.pytorch.org/whl/nightly/cu117
-$PYTHON -m pip install transformers
+python -m pip install numpy --pre torch[dynamo] --force-reinstall --extra-index-url https://download.pytorch.org/whl/nightly/cu117
+python -m pip install transformers
 
 
 echo "Activate venv with:"
--- torch_inductor_test.py
+++ torch_inductor_test.py
@@ -6,16 +6,20 @@ import time
 
 def benchmark():
     torch._dynamo.config.verbose = True
-    #torch.set_default_tensor_type(torch.cuda.FloatTensor)
-    #torch.backends.cuda.matmul.allow_tf32 = True
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)
+    torch.backends.cuda.matmul.allow_tf32 = True
     #torch.set_float32_matmul_precision('high')
-    #torch.set_float32_matmul_precision('medium')
+    torch.set_float32_matmul_precision('medium')
 
     model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
+    model = dynamo.optimize("inductor")(model)
     test_input = torch.randint(2, (1, 128))
 
-    #frontend_model = torch.compile(frontend_model, mode="max-autotune", backend="inductor")
-    #frontend_model = torch.compile(frontend_model, backend="inductor")
+    model.to("cuda")
+    test_input.to("cuda")
+
+    #model = torch.compile(model, mode="max-autotune", backend="inductor")
+    #model = torch.compile(model, backend="inductor")
 
     for i in range(5):
         begin = time.time()
